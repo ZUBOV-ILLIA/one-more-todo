c57470a8632446d6e64b9cf9a5d15e7db0c4282a
ZUBOV ILLIA <zubovillia@gmail.com>
2024-08-18 18:28:22 +0300

    added btn to show input, transitions and more ...

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,7 @@ export default function Home() {
   const inpRef = useRef<any>(null);
   const [todo, setTodo] = useState<TodoType>({ userId: 1, id: '', title: '', completed: false });
   const [todos, setTodos] = useState<TodoType[]>([]);
+  const [isVisibleCreateTodo, setIsVisibleCreateTodo] = useState(false);
 
   useEffect(() => {
     axios.get(`${API}/todos`)
@@ -41,6 +42,7 @@ export default function Home() {
 
         setTodos([...todos, res.data]);
         setTodo({ userId: 1, id: '', title: '', completed: false });
+        setIsVisibleCreateTodo(false);
       })
       .catch(err => {
         console.log('err', err);
@@ -64,26 +66,42 @@ export default function Home() {
       }
 
       <div
-        className="fixed bottom-4 right-2 inline-flex justify-center items-center w-16 h-16  bg-indigo-700 text-black rounded-full text-4xl pb-1"
+        className={`duration-300 ${isVisibleCreateTodo ? 'opacity-0 invisible translate-x-11' : ''} fixed bottom-4 right-2 inline-flex justify-center items-center w-16 h-16  bg-indigo-700 text-black rounded-full text-4xl pb-1`}
+        onClick={() => setIsVisibleCreateTodo(true)}
       >
         +
       </div>
 
-      {/* <div className="sticky bottom-0 left-0 right-0 bg-black pt-3 pb-6">
+      <div
+        className={`duration-300 ${isVisibleCreateTodo ? 'opacity-50' : 'opacity-0 invisible'} fixed top-0 bottom-0 left-0 right-0 flex bg-black`}
+        onClick={() => setIsVisibleCreateTodo(false)}
+      ></div>
 
-          <input
-            ref={inpRef}
-            className="text-xl w-full rounded-s outline-none pl-2"
-            type="text"
-            name="todoInput"
-            value={todo?.title}
-            onChange={e => setTodo({ ...todo, title: e.target.value })}
-            onKeyDown={(e) => {
-              if (e.key === 'Enter') createTodo();
-            }}
-            style={{ color: 'black' }}
-          />
-      </div> */}
+      <div className={`duration-300 ${isVisibleCreateTodo ? '' : 'translate-y-11 opacity-0 invisible'} fixed bottom-0 left-0 right-0 p-3 flex bg-neutral-800`}>
+        <input
+          type="checkbox"
+          className="mr-2"
+          disabled
+        />
+        <input
+          ref={inpRef}
+          className="text-xl w-full rounded outline-none px-2 bg-neutral-800 text-slate-100"
+          type="text"
+          name="todoInput"
+          value={todo?.title}
+          placeholder="Добавить задачу"
+          onChange={e => setTodo({ ...todo, title: e.target.value })}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') createTodo();
+          }}
+        />
+        <button
+          className={`bg-slate-${todo.title.trim() ? '100' : '400'} text-neutral-800 min-w-7 min-h-7 rounded`}
+          onClick={createTodo}
+        >
+        ↑
+        </button>
+      </div>
 
     </main>
   );
